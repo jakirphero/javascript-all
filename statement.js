703d707b00341b50aa0f6b9df3a72393b1be174b
jakir <mdjakirhossain2235@gmail.com>
2023-10-21 09:35:00 +0600

    add statement syntax operators

---
+++ statement.js
@@ -0,0 +1,57 @@
+//js statement on every line
+//js statement composed (values, operators, expressions, keywords, comments, Semicolons, White Space)
+//js try keyword (js error handling keyword)
+//js return keyword (this keyword exit function)
+// 1.
+let x, y, z;
+//2.
+x = 10;
+//3.
+y = 20;
+//4.
+z = x + y ;
+//5.
+// console.log(z);
+
+//js syntax
+//js value (fixed value-Literals, variable value-Variables)
+10.10
+"jakir hossain"
+'jk bd'
+let a, b;
+a = 5
+b = 4
+//js arithmetic operators ( +, -, *, **, /, %, ++, -- )
+const arithmeticOperators = 100 + 100 * a
+// Comparison Operators 
+
+// String Operators
+
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+// js Case Sensitive (Hyphens, Underscore, Upper Camel Case, Lower Camel Case)
+//first-name js not allowed hyphens
+const first_name = "jakir";
+const LastName = "hossain";
+const surename = "jk"
+
+
+//js Comparison Operators
+//js operators are definde (true and false)
+//1. == equal to
+//2. ===	equal value and equal type
+//3. !=	not equal
+//4. !==	not equal value or not equal type
+//5. >	greater than
+//6. <	less than
+//7. >=	greater than or equal to
+//8. <=	less than or equal to
+//9. ?	ternary operator (conditional ternary operator (condition ? exprIfTrue : exprIfFalse))
+
+//js logical operators
+//1. &&	logical and
+//2. ||	logical or
+//3. !	logical not
+
